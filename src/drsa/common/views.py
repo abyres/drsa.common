9dd61df6c956ab61c800ae262bf4625b2b4e602a
Izhar Firdaus <kagesenshi.87@gmail.com>
2016-01-06 16:32:22 +0800

    add default home view

--- src/drsa/common/views.py
+++ src/drsa/common/views.py
@@ -6,4 +6,14 @@ from wraptor.decorators import memoize
 from sqlalchemy.sql import text
 
 class Views(BaseViews):
-    pass
+
+    @view_config(route_name='drsa.home',
+            renderer='templates/home.pt',
+            permission='pysiphae.View')
+    def home_view(self):
+        items = self.main_navigation
+        dashboards = [{'title': i['label'], 'url': i['href']} for i in items]
+        return {
+            'dashboards': dashboards
+        }
+
