d8c32de16db37e9b9c900846d84e37a91a5eec2b
Izhar Firdaus <kagesenshi.87@gmail.com>
2016-01-15 18:26:04 +0800

    update to latest pysiphae API

--- src/drsa/common/views.py
+++ src/drsa/common/views.py
@@ -1,19 +1,16 @@
 from pyramid.view import view_config
-from pysiphae.views import Views as BaseViews
 from sqlalchemy.engine import create_engine
 from pyramid.exceptions import NotFound
 from wraptor.decorators import memoize
 from sqlalchemy.sql import text
 
-class Views(BaseViews):
-
-    @view_config(route_name='drsa.home',
-            renderer='templates/home.pt',
-            permission='pysiphae.View')
-    def home_view(self):
-        items = self.main_navigation
-        dashboards = [{'title': i['label'], 'url': i['href']} for i in items]
-        return {
-            'dashboards': dashboards
-        }
+@view_config(route_name='drsa.home',
+        renderer='templates/home.pt',
+        permission='pysiphae.View')
+def home_view(context, request):
+    items = request.main_navigation
+    dashboards = [{'title': i['label'], 'url': i['href']} for i in items]
+    return {
+        'dashboards': dashboards
+    }
 
